72798616b849ef118cfa82b84dbfdef83a229c77
Julien Catania <julien.catania@gmail.com>
2023-08-30 21:45:08 +0200

    adding eslint + using commander

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('Hello, world! I\'m a eMusk')
+import { Command } from 'commander';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { name, description, version } = require('../package.json');
+
+const program = new Command();
+
+program
+  .name(name)
+  .description(description)
+  .version(version)
+  .action(() => {
+    console.log('coucou');
+  });
+
+program.parse();
