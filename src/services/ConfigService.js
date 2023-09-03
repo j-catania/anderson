a54a833c7692e73af26da65edde6b4c4d2cb8b8c
Julien Catania <julien.catania@gmail.com>
2023-09-03 15:28:03 +0200

    more secure config file reading

--- src/services/ConfigService.js
+++ src/services/ConfigService.js
@@ -5,7 +5,15 @@ import { readFile } from 'node:fs/promises';
  * @param path
  * @returns {ConfigFile}
  */
-const get = async (path) => JSON.parse(await readFile(path, 'utf-8'));
+const get = async (path) => {
+  try {
+    const cfg = await readFile(path, 'utf-8');
+
+    return JSON.parse(cfg);
+  } catch (e) {
+    throw new Error(`Error reading config file: ${e.message}`);
+  }
+};
 
 const ConfigService = {
   get,
