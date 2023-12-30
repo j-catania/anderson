acc85d2aa05712988594feeeadeef1706caee46b
Julien Catania <julien.catania@gmail.com>
2023-12-30 15:07:23 +0100

    :sparkles: adding prometheus with default metrics

--- src/services/Express.js
+++ src/services/Express.js
@@ -1,21 +1,33 @@
 import express from 'express'
+import client from 'prom-client'
 
-let app
 /**
  * @param {number} port
  * @param {ServiceStatus[]} serviceStatuses
- * @returns {Server}
  */
 const init = (port, serviceStatuses) => {
-  app = express()
+  const app = express()
 
-  app.get('/', function (req, res) {
+  const register = new client.Registry()
+  register.setDefaultLabels({
+    app: 'emusk'
+  })
+  client.collectDefaultMetrics({ register })
+
+  app.get('/', (req, res) => {
     res.send(serviceStatuses)
   })
-
-  return app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
+  app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType)
+    res.send(await register.metrics())
   })
+
+  const server = app.listen(port, () => {
+    console.log(`Service statuses are available on http://localhost:${port}`)
+    console.log(`Metrics are available on http://localhost:${port}/metrics`)
+  })
+
+  return {server}
 }
 
 const Express = {
